33bd9311513bb8a99ac74dbd7d8c7a2ebc963e5a
ak7an <nicholstribe@gmail.com>
2026-05-21 10:36:04 -0600

    Add D-Star rollover and EOT validation tools

---
+++ multimode-reflector/tools_dstar_rollover.cpp
@@ -0,0 +1,61 @@
+#include <arpa/inet.h>
+#include <cstring>
+#include <iostream>
+#include <sys/socket.h>
+#include <unistd.h>
+
+static void sendVoice(int sock,
+                      const sockaddr_in& addr,
+                      uint8_t seq)
+{
+    uint8_t frame[27];
+
+    memset(frame, 0, sizeof(frame));
+
+    memcpy(frame, "DSVT", 4);
+
+    frame[12] = 0x12;
+    frame[13] = 0x34;
+
+    frame[14] = seq;
+
+    for (int i = 15; i < 27; ++i)
+        frame[i] = i;
+
+    sendto(sock,
+           frame,
+           sizeof(frame),
+           0,
+           reinterpret_cast<const sockaddr*>(&addr),
+           sizeof(addr));
+
+    std::cout << "Sent sequence "
+              << static_cast<int>(seq)
+              << "\n";
+
+    usleep(100000);
+}
+
+int main() {
+
+    int sock =
+        socket(AF_INET, SOCK_DGRAM, 0);
+
+    sockaddr_in addr{};
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(9000);
+
+    inet_pton(AF_INET,
+              "127.0.0.1",
+              &addr.sin_addr);
+
+    sendVoice(sock, addr, 19);
+    sendVoice(sock, addr, 20);
+    sendVoice(sock, addr, 0);
+    sendVoice(sock, addr, 1);
+
+    close(sock);
+
+    return 0;
+}
