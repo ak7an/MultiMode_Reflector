95aa2c28d27ea6513edc89b77cee4690f8471be4
ak7an <nicholstribe@gmail.com>
2026-05-27 13:01:30 -0600

    Restore D-Star encoder bridge path

--- multimode-reflector/src/protocol/dstar_network_frame.h
+++ multimode-reflector/src/protocol/dstar_network_frame.h
@@ -1,18 +1,21 @@
 #pragma once
 
-#include "../core/media_frame.h"
-
 #include <vector>
 #include <cstdint>
+#include <cstddef>
 
-class DStarNetworkFrame {
+#include "core/media_frame.h"
+
+class DStarNetworkFrame
+{
 public:
-
     static std::vector<uint8_t> build(
-        const MediaFrame& frame);
+        const MediaFrame& frame
+    );
 
     static bool parse(
         const uint8_t* data,
         size_t length,
-        MediaFrame& frame);
+        MediaFrame& frame
+    );
 };
