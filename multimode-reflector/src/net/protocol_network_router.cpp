c461272af6a969dd76d5e6c24d1d64b818272b0d
ak7an <nicholstribe@gmail.com>
2026-05-28 12:57:24 -0600

    Add UDP send support to protocol network router

--- multimode-reflector/src/net/protocol_network_router.cpp
+++ multimode-reflector/src/net/protocol_network_router.cpp
@@ -1,6 +1,10 @@
 #include "protocol_network_router.h"
 
+#include <unistd.h>
+#include <sys/socket.h>
+
 #include "../core/logger.h"
+#include "../protocol/protocol_name.h"
 
 ProtocolNetworkRouter::ProtocolNetworkRouter(
     ProtocolPeerRegistry& registry) :
@@ -17,9 +21,58 @@ void ProtocolNetworkRouter::routePacket(
 
     Logger::log(INFO,
         "ProtocolNetworkRouter route: PROTO=" +
-        std::to_string(static_cast<int>(proto)) +
+        ProtocolName::toString(proto) +
         " PEERS=" +
         std::to_string(peers.size()) +
         " LEN=" +
         std::to_string(packet.size()));
+
+    for (const auto& peer : peers)
+    {
+        int sockfd =
+            socket(AF_INET, SOCK_DGRAM, 0);
+
+        if (sockfd < 0)
+        {
+            Logger::log(ERROR,
+                "ProtocolNetworkRouter socket create failed");
+
+            continue;
+        }
+
+        ssize_t sent =
+            sendto(
+                sockfd,
+                packet.data(),
+                packet.size(),
+                0,
+                reinterpret_cast<const sockaddr*>(
+                    &peer.address),
+                sizeof(peer.address));
+
+        if (sent < 0)
+        {
+            Logger::log(ERROR,
+                "ProtocolNetworkRouter send failed: PROTO=" +
+                ProtocolName::toString(proto) +
+                " HOST=" +
+                peer.host +
+                " PORT=" +
+                std::to_string(peer.port));
+        }
+        else
+        {
+            Logger::log(INFO,
+                "ProtocolNetworkRouter sent: PROTO=" +
+                ProtocolName::toString(proto) +
+                " HOST=" +
+                peer.host +
+                " PORT=" +
+                std::to_string(peer.port) +
+                " LEN=" +
+                std::to_string(sent));
+        }
+
+        close(sockfd);
+    }
 }
