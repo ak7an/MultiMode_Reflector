722cf85c664c5b54faca989fff2e3c1fea86b906
ak7an <nicholstribe@gmail.com>
2026-05-28 12:50:16 -0600

    Add configured XLXD peer registrar

---
+++ multimode-reflector/src/net/xlxd_peer_registrar.cpp
@@ -0,0 +1,52 @@
+#include "xlxd_peer_registrar.h"
+
+#include <arpa/inet.h>
+
+#include "../core/xlxd_peer_config.h"
+#include "../core/logger.h"
+
+void XLXDPeerRegistrar::registerConfiguredPeer(
+    ProtocolPeerRegistry& registry)
+{
+    if (!XLXDPeerConfig::enabled())
+    {
+        Logger::log(INFO,
+            "XLXD peering disabled");
+
+        return;
+    }
+
+    ProtocolPeer peer;
+
+    peer.protocol =
+        ProtocolType::DSTAR;
+
+    peer.host =
+        XLXDPeerConfig::host();
+
+    peer.port =
+        XLXDPeerConfig::port();
+
+    peer.address.sin_family =
+        AF_INET;
+
+    peer.address.sin_port =
+        htons(peer.port);
+
+    inet_pton(
+        AF_INET,
+        peer.host.c_str(),
+        &peer.address.sin_addr);
+
+    registry.addPeer(peer);
+
+    Logger::log(INFO,
+        "Configured XLXD peer registered: REFLECTOR=" +
+        XLXDPeerConfig::reflector() +
+        " HOST=" +
+        peer.host +
+        " PORT=" +
+        std::to_string(peer.port) +
+        " MODULE=" +
+        std::string(1, XLXDPeerConfig::module()));
+}
---
+++ multimode-reflector/src/net/xlxd_peer_registrar.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "protocol_peer_registry.h"
+
+class XLXDPeerRegistrar
+{
+public:
+
+    static void registerConfiguredPeer(
+        ProtocolPeerRegistry& registry);
+};
